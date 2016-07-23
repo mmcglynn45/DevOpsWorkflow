0ff329211b8efa590122721d4abb22078f290d71
Matt <ndmmcglynn@gmail.com>
2016-07-23 13:15:59 -0700

    Split templates

---
+++ DevOps/gitlabInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install curl openssh-server ca-certificates postfix
+
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+sudo apt-get install gitlab-ce
+
+sudo gitlab-ctl reconfigure
