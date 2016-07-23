a5f2ee3b3b3987ffa9fd9df15897bf5fa31ef1c2
Matt <ndmmcglynn@gmail.com>
2016-07-23 15:38:26 -0700

    gitLab modifications

--- DevOps/gitlabInstall.sh
+++ DevOps/gitlabInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install curl openssh-server ca-certificates postfix
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install curl openssh-server ca-certificates postfix
 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
