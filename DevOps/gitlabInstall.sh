95c09112ff017aab89273a2c310d285814bb895d
Matt <ndmmcglynn@gmail.com>
2016-07-24 00:52:24 -0700

    go

--- DevOps/gitlabInstall.sh
+++ DevOps/gitlabInstall.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+
+dns=$1
+
+
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y install curl openssh-server ca-certificates postfix
+apt-get -y install curl openssh-server ca-certificates 
 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
 sudo apt-get install gitlab-ce
+
+sed '1,/external_url/d' /etc/gitlab/gitlab.rb -i
+echo "external_url http://$dns/" >> /etc/gitlab/gitlab.rb
+
 
 sudo gitlab-ctl reconfigure
